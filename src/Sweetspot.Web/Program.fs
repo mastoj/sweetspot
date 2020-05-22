09d71396a95f5e9986a3b2b5b889d7eb9aedaa69
Tomas Jansson <tomas.jansson@elkjop.no>
2020-05-22 22:22:45 +0200

    Add hello web app

---
+++ src/Sweetspot.Web/Program.fs
@@ -0,0 +1,50 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Giraffe
+open Saturn
+open Microsoft.AspNetCore.Http
+open FSharp.Control.Tasks.ContextInsensitive
+
+type Hello = { Wat: string }
+
+let messagesHandler next (ctx: HttpContext) =
+    task {
+        let! hello = ctx.BindJsonAsync<Hello>()
+        let response = { Wat = (sprintf "Hello again %s" hello.Wat) }
+        return! ctx.WriteJsonAsync response
+    }
+
+let apiRoutes = router {
+    post "/api/messages" messagesHandler    
+}
+
+let webRoutes = router {
+    get "/" (text "Hello world")
+}
+
+let app = application {
+    use_router apiRoutes
+    use_router webRoutes
+}
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    run app
+    0 // return an integer exit code
+
+(*
+
+http://localhost:5000/
+http://localhost:5000/api/messages
+
+POST http://localhost:5000/api/messages HTTP/1.1
+content-type: application/json
+
+{
+    "wat": "sample two"
+}
+
+*)

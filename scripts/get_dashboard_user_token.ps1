6a27e70ce887ab147449c0448ef867c5b8d51e6c
Tomas Jansson <tomas.jansson@elkjop.no>
2020-05-25 09:30:39 +0200

    Script for dashboard token

---
+++ scripts/get_dashboard_user_token.ps1
@@ -0,0 +1,1 @@
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | sls admin-user | ForEach-Object { $_ -Split '\s+' } | Select -First 1)
---
+++ scripts/get_dashboard_user_token.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')

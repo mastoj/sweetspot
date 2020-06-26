237ec560276f5a9db88452accdd1eb974018407e
Tomas Jansson <tomas.jansson@gmail.com>
2020-06-27 00:25:45 +0200

    Only resource group

--- src/infrastructure/Sweetspot.Infrastructure.Application/Program.fs
+++ src/infrastructure/Sweetspot.Infrastructure.Application/Program.fs
@@ -1,12 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open KubernetesHelpers
 open Pulumi.Azure.Core
 open Pulumi.FSharp
 open System
 
 let infra () =
-    let stack = getCoreStackRef() |> ignore
+//    let stack = getCoreStackRef() |> ignore
     let resourceGroup = ResourceGroup("Deleteme")
     [] |> dict
     // let infrastructureOutput = deployAppInfrastructure stack
